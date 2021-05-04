a65aa80af9de81cc52df483459682ab54c1ed235
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-04 22:02:19 +0900

    chore: 設定ファイル

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extension: ['ts'],
-  timeout: '20s',
-  require: 'ts-node/register',
-};
-/**
- * @url https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.json
- */
