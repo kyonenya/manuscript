e81463e4dd0cc94070b745f47efabb2f33bdf207
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-08 22:11:07 +0900

    feat: chakra uiの導入

---
+++ components/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const fonts = { mono: `'Menlo', monospace` };
+
+const breakpoints = createBreakpoints({
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
+});
+
+const fontWeights = {
+  normal: 400,
+  medium: 600,
+  bold: 700,
+};
+
+export const theme = extendTheme({
+  colors: {
+    black: '#16161D',
+  },
+  fonts,
+  fontWeights,
+  breakpoints,
+});
