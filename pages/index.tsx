cd508ec6607bb712a3a07731e4a28569e031d0ca
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-09 13:15:48 +0900

    feat: 記事リストの2列表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import Head from 'next/head';
-import { Box, Container, Stack, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  SimpleGrid,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { Entry } from '../app/Entry';
 import { PostListItem } from '../components/PostListItem';
 
@@ -11,19 +17,19 @@ export default function Index() {
   });
 
   return (
-    <div className="container">
+    <Box bg={useColorModeValue('gray.100', 'gray.700')}>
       <Head>
         <title>manuscript</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box bg={useColorModeValue('gray.100', 'gray.700')}>
-        <Container maxW={'5xl'} py={4} as={Stack} spacing={12}>
-          <Stack spacing={{ base: 4, md: 2, lg: 8 }}>
-            <PostListItem entry={entry1} />
-            <PostListItem entry={entry1} />
-          </Stack>
-        </Container>
-      </Box>
-    </div>
+      <Container maxW="4xl" py={8}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 4, lg: 8 }}>
+          <PostListItem entry={entry1} />
+          <PostListItem entry={entry1} />
+          <PostListItem entry={entry1} />
+          <PostListItem entry={entry1} />
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 }
