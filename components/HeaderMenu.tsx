c6f5c5abe2319adb7eeb0ab33e7493bfac6c1112
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-09 16:36:52 +0900

    feat: ヘッダーメニューの試作

---
+++ components/HeaderMenu.tsx
@@ -0,0 +1,75 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  InputGroup,
+  Input,
+  InputRightAddon,
+  Link,
+  Stack,
+  useDisclosure,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { ArrowBackIcon, SearchIcon, SettingsIcon } from '@chakra-ui/icons';
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}
+  >
+    {children}
+  </Link>
+);
+
+export const TopHeaderMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box px={4} bg={useColorModeValue('gray.100', 'gray.900')} boxShadow={'md'}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={<ArrowBackIcon />}
+          aria-label={'Back to Top'}
+        />
+
+        <HStack spacing={8} alignItems={'center'}>
+          <InputGroup size="md">
+            <Input
+              type="text"
+              placeholder="Enter Search Word"
+              aria-label="Serach Form"
+              borderColor={useColorModeValue('gray.300', 'gray.700')}
+            />
+            <InputRightAddon
+              p={1}
+              borderColor={useColorModeValue('gray.300', 'gray.700')}
+            >
+              <IconButton
+                size="sm"
+                aria-label="検索"
+                icon={<SearchIcon />}
+              ></IconButton>
+            </InputRightAddon>
+          </InputGroup>
+        </HStack>
+
+        <Flex alignItems={'center'}>
+          <IconButton
+            icon={<SettingsIcon />}
+            aria-label="設定"
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import {
 } from '@chakra-ui/react';
 import { Entry } from '../app/Entry';
 import { PostListItem } from '../components/PostListItem';
+import { TopHeaderMenu } from '../components/HeaderMenu';
 
 export default function Index() {
   const entry1 = new Entry({
@@ -15,21 +16,31 @@ export default function Index() {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.',
     tags: ['タグ'],
   });
+  const entry2 = new Entry({
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.',
+    tags: null,
+  });
 
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.700')}>
+    <>
       <Head>
         <title>manuscript</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container maxW="4xl" py={8}>
+      <TopHeaderMenu />
+      <Container
+        maxW="4xl"
+        py={4}
+        bg={useColorModeValue('gray.100', 'gray.700')}
+      >
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 4, lg: 8 }}>
           <PostListItem entry={entry1} />
-          <PostListItem entry={entry1} />
-          <PostListItem entry={entry1} />
+          <PostListItem entry={entry2} />
+          <PostListItem entry={entry2} />
           <PostListItem entry={entry1} />
         </SimpleGrid>
       </Container>
-    </Box>
+    </>
   );
 }
