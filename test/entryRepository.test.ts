a01dc2e46216898e6f02366b7460e2f2c06580b3
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-12 21:16:32 +0900

    feat: テストデータを追加してからqueryをテスト

--- test/entryRepository.test.ts
+++ test/entryRepository.test.ts
@@ -3,8 +3,19 @@ import * as entryRepository from '../app/entryRepository';
 import { begin, rollback } from '../app/postgres';
 import { toEntry } from '../app/Entry';
 
-describe('entriesRepository', () => {
-  beforeEach(() => begin());
+describe('queries_entriesRepository', () => {
+  before(async () => {
+    await begin();
+    const entry1 = toEntry({
+      text: 'これは一つ目のサンプルの記事です。',
+      tags: ['タグ1', 'タグ2'],
+    });
+    const entry2 = toEntry({
+      text: 'これは二つ目のサンプルの記事です。',
+    });
+    await entryRepository.createOne({ entry: entry1 });
+    await entryRepository.createOne({ entry: entry2 });
+  });
 
   it('readMany', async () => {
     const entries = await entryRepository.readMany({ limit: 1 });
@@ -16,6 +27,12 @@ describe('entriesRepository', () => {
     const entries = await entryRepository.findByKeyword({ keyword, limit: 1 });
     assert.strictEqual(entries.length, 1);
   });
+
+  after(() => rollback());
+});
+
+describe('mutation_entriesRepository', () => {
+  beforeEach(() => begin());
 
   it('createOne', async () => {
     const entry = toEntry({
