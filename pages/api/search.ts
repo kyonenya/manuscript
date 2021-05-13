07eb1ae3db4b9da24635624e6b1af0085234e29b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-13 15:41:43 +0900

    refactor: apiをPOSTオンリーのrpcに

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,10 +1,16 @@
-import { NextApiHandler } from 'next';
+import { NextApiHandler, NextApiRequest } from 'next';
 import { findByKeyword } from '../../app/entryRepository';
 
+const validateRequest = (reqBody: NextApiRequest['body']) => {
+  return {
+    keyword: reqBody.keyword.toString(),
+    limit: parseInt(reqBody.limit.toString(), 10),
+    offset: parseInt(reqBody.offset?.toString() ?? 0, 10),
+  };
+};
+
 const handler: NextApiHandler = async (req, res) => {
-  const keyword = decodeURIComponent(req.query.q.toString());
-  const limit = parseInt(req.query.limit.toString(), 10);
-  const entries = await findByKeyword({ keyword, limit });
+  const entries = await findByKeyword(validateRequest(req.body));
   await res.json(entries);
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,15 +14,18 @@ import { PostList } from '../components/PostList';
 import { TopHeaderMenu } from '../components/HeaderMenu';
 
 export default function Index(props: { entries: Entry[] }) {
-  const { data } = useQuery(['search', { keyword: '演技' }], () =>
-    fetcher(`/api/search?q=${encodeURIComponent('演技')}&limit=3`)
-  );
-  const fetcher = (url: string) =>
-    fetch(url, {
-      method: 'GET',
+  const { data, error } = useQuery(['search', { keyword: '演技' }], () =>
+    fetch('/api/search', {
+      method: 'POST',
+      body: JSON.stringify({
+        keyword: '演技',
+        limit: 5,
+        //        offset: 5,
+      }),
       headers: new Headers({ 'Content-Type': 'application/json' }),
       credentials: 'same-origin',
-    }).then((res) => res.json());
+    }).then((res) => res.json())
+  );
 
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.700')}>
