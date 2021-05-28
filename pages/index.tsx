55f41a2b66ef3e35b814cec45fef92a45c7f71c8
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-28 22:12:43 +0900

    feat: intersection observerを用いた無限スクロール・hooksのeslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,24 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { TopHeaderMenu } from '../components/HeaderMenu';
 import { PostList } from '../components/PostList';
 import { useSearchEntries } from '../hooks/useSearchEntries';
 
-const limit = 3;
+const limit = 5;
 
 export default function Index() {
   const { data, fetchNextPage, keyword, setKeyword } = useSearchEntries({
     limit,
   });
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (!inView) return;
+    fetchNextPage();
+  }, [inView, fetchNextPage]);
 
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.700')}>
@@ -29,16 +37,16 @@ export default function Index() {
           setKeyword(keyword === '' ? undefined : keyword)
         }
       />
-      <Box align="center">
-        <IconButton
-          aria-label={'もっと読む'}
-          onClick={() => fetchNextPage()}
-          icon={<AddIcon />}
-          size={'sm'}
-        />
-      </Box>
       <Container maxW="4xl" py={4}>
         {data && <PostList entries={data.pages.flat()} keyword={keyword} />}
+        <Box align="center" ref={ref}>
+          <IconButton
+            aria-label={'もっと読む'}
+            onClick={() => fetchNextPage()}
+            icon={<AddIcon />}
+            size={'sm'}
+          />
+        </Box>
       </Container>
     </Box>
   );
