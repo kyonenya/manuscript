44a9e210f4b4c92ad1fdd465dcf0cf67bbb03592
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-06-08 12:18:45 +0900

    fix: apiFactory

--- infra/apiFactory.ts
+++ infra/apiFactory.ts
@@ -3,13 +3,15 @@ import { NextApiHandler } from 'next';
 import { z } from 'zod';
 
 export const apiFactory = (
-  usecase: (input: any /* Input */) => any /* Output */,
+  usecase: (input: any /* Input */) => Promise<any /* Output */>,
   request: z.ZodObject<any /* Input */>
 ): NextApiHandler => {
   const handler: NextApiHandler = async (req, res) => {
     const parsed = request.safeParse(req.body);
     if (!parsed.success) return res.status(400).json({ error: parsed.error });
-    return res.json((await usecase(parsed.data /* Input */)) ?? { ok: true });
+
+    const result /* Output */ = await usecase(parsed.data /* Input */);
+    return res.json(result ?? { ok: true });
   };
   return handler;
 };
