6b4c7df1312f996b92b29c7d38c0b70a55236adb
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-12-05 23:07:36 +0900

    refactor: ドメインモデルを自作したsearch-summaryパッケージに置き換え

--- domain/SearchSummary.ts
+++ domain/SearchSummary.ts
@@ -1,34 +1,14 @@
-export type searchSummary = {
-  isBeforeEllipsed: boolean;
-  beforeText: string;
-  keyword: string;
-  afterText: string;
-  isAfterEllipsed: boolean;
-};
+import { generateSummaryEntity, SummaryEntity } from 'search-summary';
+
+export type searchSummary = SummaryEntity;
 
 export const newSearchSummary = (props: {
   keyword: string;
   text: string;
 }): searchSummary => {
   const { keyword, text } = props;
-  const wholeLength = 100;
-  const beforeLength = 40;
-
-  const afterLength = wholeLength - beforeLength - keyword.length;
-  const keywordIndex = text.indexOf(keyword);
-  const beforeIndex = keywordIndex - beforeLength;
-  const afterIndex = keywordIndex + keyword.length;
-  const isNearTop = beforeIndex <= 0;
-
-  return {
-    isBeforeEllipsed: !isNearTop,
-    beforeText: isNearTop
-      ? text.substr(0, keywordIndex)
-      : text.substr(beforeIndex, beforeLength),
-    keyword: text.substr(keywordIndex, keyword.length),
-    afterText: isNearTop
-      ? text.substr(afterIndex, wholeLength - afterIndex)
-      : text.substr(afterIndex, afterLength),
-    isAfterEllipsed: beforeIndex + wholeLength < text.length,
-  };
+  return generateSummaryEntity(text, keyword, {
+    maxLength: 100,
+    beforeLength: 40,
+  }) as searchSummary; // TODO: allow undefined
 };
