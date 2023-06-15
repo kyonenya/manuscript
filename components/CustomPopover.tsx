5472adfb25eccc6af84352f6e4fd625eca72bdaa
kyonenya <62150154+kyonenya@users.noreply.github.com>
2023-06-15 08:03:30 +0000

    refactor: PopOverのRadix UI化

--- components/CustomPopover.tsx
+++ components/CustomPopover.tsx
@@ -1,32 +1,22 @@
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow,
-  PopoverCloseButton,
-  Placement,
-  Stack,
-} from '@chakra-ui/react';
-import { ReactElement, ReactNode } from 'react';
+import { Root, Trigger, Content, Close } from '@radix-ui/react-popover'
 
-export const CustomPopover = (props: {
-  triggerButton: ReactElement;
-  children: ReactNode;
-  placement?: Placement;
-}) => {
-  return (
-    <Popover placement={props.placement}>
-      <PopoverTrigger>{props.triggerButton}</PopoverTrigger>
-      <PopoverContent w={{ base: '90vw', md: 'sm' }}>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverBody>
-          <Stack direction="column" spacing={4} px={2} py={4}>
-            {props.children}
-          </Stack>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
-  );
-};
+type Placement = 'top' | 'right' | 'bottom' | 'left';
+
+type CustomPopoverProps = {
+  triggerButton: React.ReactNode
+  children: React.ReactNode
+  placement?: Placement
+}
+
+export const CustomPopover = ({ triggerButton, children, placement }: CustomPopoverProps) => (
+      <Root className="z-[5000]">
+    <Trigger className="cursor-pointer">{triggerButton}</Trigger>
+    <Content 
+      side={placement}
+      className="w-11/12 md:w-64 bg-white border border-gray-200 rounded shadow-lg transition duration-200 ease-in-out transform-gpu z-[5000]"
+    >
+      <Close className="absolute top-2 right-2 cursor-pointer">Close</Close>
+      <div className="p-4 space-y-4 z-[5000]">{children}</div>
+    </Content>
+  </Root>
+)
