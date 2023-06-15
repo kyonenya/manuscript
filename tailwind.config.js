c2219d8f7ab7f9b24058eab5eef59d450b260c1d
Takashi Kyonenya <kyon.musica@gmail.com>
2023-06-16 03:50:16 +0900

    refactor: メディアクエリをtailwind.configに追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '30em', // equivalent to 480px
+      md: '42em', // equivalent to 672px
+      lg: '62em', // equivalent to 992px
+      xl: '80em', // equivalent to 1280px
+    },
     extend: {},
   },
   plugins: [],
