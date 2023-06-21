38dd8c2c0fbfb36f071df4ed5f4bc18dfa033676
Takashi Kyonenya <kyon.musica@gmail.com>
2023-06-22 02:48:27 +0900

    feat: ログアウトボタン：<form>タグで囲わないとServer Actionsにならない

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import {
+  createServerActionClient,
+  createServerComponentClient,
+} from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { PostListHeader } from './PostListHeader';
+import { Button } from './_components/Button';
+import { HeaderContainer } from './_components/HeaderContainer';
 
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies });
@@ -10,5 +17,25 @@ export default async function Index() {
 
   if (!session) redirect('/login');
 
-  return <h1>Hello, Next.js!</h1>;
+  const handleSignOut = async () => {
+    'use server';
+    const supabase = createServerActionClient({ cookies });
+    await supabase.auth.signOut();
+
+    revalidatePath('/login');
+  };
+
+  return (
+    <>
+      {/* <HeaderContainer>
+        <PostListHeader />
+      </HeaderContainer> */}
+
+      <form>
+        <Button type="submit" formAction={handleSignOut}>
+          Sign Out
+        </Button>
+      </form>
+    </>
+  );
 }
