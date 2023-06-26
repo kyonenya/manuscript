95acf7dfe186500691736450647894866f380a24
Takashi Kyonenya <kyon.musica@gmail.com>
2023-06-26 11:03:04 +0900

    feat: エラーページを404の色違いとして実装

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,14 @@
 'use client'; // Error components must be Client Components
 
 import { useEffect } from 'react';
+import { SimpleHeader } from './SimpleHeader';
+import { Button } from './_components/Button';
 
-/** @see https://nextjs.org/docs/app/building-your-application/routing/error-handling */
+/**
+ * エラーページ
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/routing/error-handling
+ */
 export default function Error({
   error,
   reset,
@@ -16,16 +22,28 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>{error.message}</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <>
+      <SimpleHeader />
+      <div className="container mx-auto my-auto flex items-center px-6 py-12 lg:py-20">
+        <div className="mx-auto flex max-w-sm flex-col items-center text-center">
+          <p className="text-md font-medium text-rose-500">500 error</p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+            Internal Server Error
+          </h1>
+          <p className="mt-4 text-gray-500 dark:text-gray-400">
+            {error.message ?? 'Sorry, something went wrong.'}
+          </p>
+          <div className="mt-6 flex w-full items-center justify-center">
+            <Button
+              variant={{ color: 'danger' }}
+              className="w-auto px-5 py-2"
+              onClick={reset}
+            >
+              Try Again
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
