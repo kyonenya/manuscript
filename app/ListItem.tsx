3f8928fbeb1520034703b3f8475a9479d7222e69
kyonenya <kyon.musica@gmail.com>
2023-07-04 12:04:25 +0000

    fix: ListItemのテキストの改行を削除

--- app/ListItem.tsx
+++ app/ListItem.tsx
@@ -52,18 +52,16 @@ export const ListItem = (props: {
   onSelect: () => void;
 }) => {
   const { entry } = props;
+  const text = entry.text.replaceAll(/\n/g, ' ');
   const summary = props.searchQuery?.keyword
-    ? generateSummaryEntity(entry.text, props.searchQuery.keyword, {
+    ? generateSummaryEntity(text, props.searchQuery.keyword, {
         maxLength: 120,
         beforeLength: 50,
       })
     : undefined;
+
   const SummaryComponent = () =>
-    summary ? (
-      <SearchSummary summary={summary} />
-    ) : (
-      <Summary text={entry.text} />
-    );
+    summary ? <SearchSummary summary={summary} /> : <Summary text={text} />;
 
   return (
     <button
@@ -84,7 +82,7 @@ export const ListItem = (props: {
       <div className="mb-2" />
       <div className="mt-auto flex flex-row items-center justify-start space-x-3">
         <p className="text-gray-600 dark:text-gray-400">
-          {dayjs(entry.createdAt).format('YYYY-MM-DD')}
+          {dayjs(entry.createdAt).tz().format('YYYY-MM-DD')}
         </p>
         {props.entry.starred && (
           <StarIcon className="w-5 text-yellow-400 dark:text-yellow-500" />
