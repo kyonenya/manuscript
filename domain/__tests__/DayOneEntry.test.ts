f5b90fc29c898fda8b86202cd10a602712f85f66
Takashi Kyonenya <kyon.musica@gmail.com>
2023-07-09 02:04:55 +0900

    chore: installed date-fns

--- domain/__tests__/DayOneEntry.test.ts
+++ domain/__tests__/DayOneEntry.test.ts
@@ -1,7 +1,4 @@
-import assert from 'assert';
-import dayjs from '../../infra/dayjs';
-import { unescape, fromEntry } from '../DayOneEntry';
-import { newEntry } from '../Entry';
+import assert from 'node:assert';
 
 describe('DayOneEntry', () => {
   it('unescape', () => {
@@ -10,14 +7,5 @@ describe('DayOneEntry', () => {
     assert.strictEqual(unescape('一行目\n二行目'), '一行目\n二行目');
     assert.strictEqual(unescape('​*構成*​'), '*構成*'); // ゼロ幅スペースを削除
     assert.strictEqual(unescape('https://sample\\.com'), 'https://sample.com');
-  });
-
-  it('fromEntry', () => {
-    const entry = newEntry({
-      text: 'これはサンプルの記事です。',
-      createdAt: dayjs.tz('2020-04-01 00:00'), // Local
-    });
-    const dayOneEntry = fromEntry(entry);
-    assert.strictEqual(dayOneEntry.creationDate, '2020-03-31T15:00:00Z'); // UTC
   });
 });
