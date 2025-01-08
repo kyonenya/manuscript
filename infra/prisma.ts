7ec20a34a00a2afc59fa91ace4bc977ca4de3bee
kyonenya <kyon.musica@gmail.com>
2025-01-09 04:51:42 +0900

    fix: 本番環境でもSQLiteを参照してしまっていた問題の修正

--- infra/prisma.ts
+++ infra/prisma.ts
@@ -1,10 +1,9 @@
-
-import { createClient } from "@libsql/client/web"; // @see https://github.com/tursodatabase/libsql-client-ts/issues/184#issuecomment-2025317522
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { PrismaClient } from "@prisma/client";
+import { createClient } from '@libsql/client/web'; // @see https://github.com/tursodatabase/libsql-client-ts/issues/184#issuecomment-2025317522
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { PrismaClient } from '@prisma/client';
 
 const libsql = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? "",
+  url: process.env.TURSO_DATABASE_URL ?? '',
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 const adapter = new PrismaLibSQL(libsql);
@@ -18,6 +17,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
