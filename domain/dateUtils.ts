c38d30f4038b3c16843e9157cca6cd6b055523e6
kyonenya <kyon.musica@gmail.com>
2025-05-30 20:07:30 +0900

    fix: eslintの設定ファイル混在

--- domain/dateUtils.ts
+++ domain/dateUtils.ts
@@ -1,5 +1,5 @@
-import { formatInTimeZone } from 'date-fns-tz';
 import { parseISO } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
 
 export const formatTZ = (isoDateString: string, formatStr: string): string =>
   formatInTimeZone(parseISO(isoDateString), 'Asia/Tokyo', formatStr);
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
